97c451c032f3e7ab4057b20804aea235d0791a58
ShivakumarLS <shivakumarls0453@gmail.com>
2026-01-18 17:15:12 +0530

    Backup before implementing admin approval workflow

--- userApplication/src/main/java/com/shivu/userapplication/config/SecurityConfiguration.java
+++ userApplication/src/main/java/com/shivu/userapplication/config/SecurityConfiguration.java
@@ -98,7 +98,7 @@ public class SecurityConfiguration {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:5174", "http://localhost:3000"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
